ae208cc8039a148db59274ee27a82f579a7795f9
agung1453 <76863540+agung1453@users.noreply.github.com>
2024-12-15 23:19:51 +0700

    update Pengelola File IndexedDB/service-worker.js

---
+++ Pengelola File IndexedDB/service-worker.js
@@ -0,0 +1,39 @@
+// Saat service worker diinstal, cache file yang diperlukan
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/',              // Halaman utama
+        '/index.html',    // Halaman index
+        '/',     // File CSS (jika ada)
+        '/',     // File JavaScript (jika ada)
+        '/'       // Gambar (jika ada)
+      ]);
+    })
+  );
+});
+
+// Saat service worker diaktifkan, hapus cache lama jika ada
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['my-cache']; // Daftar cache yang boleh tetap ada
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); // Menghapus cache yang tidak digunakan
+          }
+        })
+      );
+    })
+  );
+});
+
+// Mencegat permintaan jaringan dan mengembalikan data dari cache atau jaringan
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request); // Kembalikan data dari cache atau ambil dari jaringan
+    })
+  );
+});
